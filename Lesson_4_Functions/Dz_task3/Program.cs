c059d8f8999a00a899b80fe008a5e1ddda40658d
SteelAlchemist001 <vladislavbuhal@ya.ru>
2023-12-08 16:33:59 +0300

    1

---
+++ Lesson_4_Functions/Dz_task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+// [1 3 5 6 7 8] => [8 7 6 5 3 1]
+
+int[] numbers = { 1, 3, 5, 6, 7, 8 }; // Исходный массив
+int temp;
+// Вывод исходного массива
+Console.Write("Исходный массив: ");
+foreach (int number in numbers)
+{
+    Console.Write(number + " ");
+}
+// Реверсирование массива
+for (int i = 0; i < numbers.Length / 2; i++)
+{
+    // Меняем местами элементы
+    temp = numbers[i];
+    numbers[i] = numbers[numbers.Length - 1 - i];
+    numbers[numbers.Length - 1 - i] = temp;
+}
+// Вывод измененного массива
+Console.Write("\nПеревернутый массив: ");
+foreach (int number in numbers)
+{
+    Console.Write(number + " ");
+}
+
