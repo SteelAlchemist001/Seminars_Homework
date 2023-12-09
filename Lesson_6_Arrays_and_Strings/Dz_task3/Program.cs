2936eef9e51ee91559b93d69724f231668c2cd42
SteelAlchemist001 <vladislavbuhal@ya.ru>
2023-12-09 16:25:21 +0300

    1

---
+++ Lesson_6_Arrays_and_Strings/Dz_task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да
+// “55655” => Да
+
+public static void Main(string[] args)
+{
+// Входная строка для проверки
+string input = "шалаш";
+// Вызов метода для проверки, является ли строка палиндромомbool isPalindrome = IsPalindrome(input);
+// Вывод результата
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
+}
+// Метод для проверки, является ли строка палиндромомpublic static bool IsPalindrome(string str)
+{
+// Нормализация строки путем удаления не буквенно-цифровыхсимволов и приведения к нижнему регистру
+string normalized = new
+string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+// Сравнение строки с ее перевернутым вариантом
+return normalized.SequenceEqual(normalized.Reverse());
+}
