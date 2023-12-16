c91553e6186dccc738640fe9c79d6f1ee993e959
SteelAlchemist001 <vladislavbuhal@ya.ru>
2023-12-16 15:40:50 +0300

    0

---
+++ Lesson_5_Two-Dimensional_Arrays/Dz_task3/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача 3: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// 4 3 1 => Строка с индексом 0
+// 2 6 9
+// 4 6 2 
+
+Console.WriteLine("Введите размер массива m x n и диапазон случайных значений:");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+    int tempSumLine = SumLineElements(array, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+
+Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+// int n1, n2;
+// int[,] A;
+
+// Console.Write("Введите количество строк в матрице: ");
+// n1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите количество столбцов в матрице: ");
+// n2 = int.Parse(Console.ReadLine());
+// A = new int[n1, n2];
+// Random rnd = new Random();
+
+// // Присвоение значений датчиком случайных чисел
+// for (int i = 0; i < n1; i++)
+//     for (int j = 0; j < n2; j++)
+//         A[i, j] = rnd.Next(-0, 10 + 1);
+
+// //Вывод массива
+// for (int i = 0; i < n1; i++, Console.WriteLine())
+//     for (int j = 0; j < n2; j++)
+//         Console.Write(A[i, j] + "\t");
